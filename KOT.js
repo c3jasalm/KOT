03c6e82e8079d2991bc4a5be5f71a20c5d8f3c02
Vesa <vesa.mujunen@cs.tamk.fi>
2014-11-02 00:15:15 +0200

    Added comment field and save button

--- KOT.js
+++ KOT.js
@@ -11,12 +11,15 @@ Template.body.events({
   "submit .new-task": function (event) {
       // This function is called when the new task form is submitted
       var text = event.target.text.value;
+      var comment = event.target.comment.value;
       Tasks.insert({
       text: text,
+      comment: comment,
       createdAt: new Date() // current time
     });
       // Clear form
       event.target.text.value = "";
+      event.target.comment.value = "";
       // Prevent default form submit
       return false;
     }
