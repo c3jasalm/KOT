1a02f250f02fdc61ff04ff5418d0e1eaa6e8c59f
Vesa <vesa.mujunen@cs.tamk.fi>
2014-11-10 11:29:18 +0200

    Counter has now only one button

--- counter.js
+++ counter.js
@@ -1,46 +1,48 @@
 // Storage for counter state
 CounterState = new Meteor.Collection('stateStore');
 if (Meteor.isClient) {
-	Session.setDefault("usedHours", 0);
-	Session.setDefault("usedMinutes", 0);
-	Session.setDefault("usedSeconds", 0); // Will be removed later
+	Session.setDefault('usedHours', 0);
+	Session.setDefault('usedMinutes', 0);
+	Session.setDefault('usedSeconds', 0); // Will be removed later
  
   Template.counter.helpers({
  	'timeStart': function () {
  		start = CounterState.findOne();
+ 		if (start) {										// Set current counter state
+ 			Session.set('currentState', 'on'); 
+ 		} else {
+ 			Session.set('currentState', 'off');
+ 		}
     	return start.previousState;
  	},
  	'timeStop': function () {
- 		return Session.get("stop");
+ 		return Session.get('stop');
  	},
  	'timeHours': function () {
- 		return Session.get("usedHours");
+ 		return Session.get('usedHours');
  	},
  	'timeMinutes': function () {
- 		return Session.get("usedMinutes");
+ 		return Session.get('usedMinutes');
  	},
  	'timeSeconds': function () {				// Will be removed later
- 		return Session.get("usedSeconds");
+ 		return Session.get('usedSeconds');
+ 	},
+ 	'currentState': function () {
+ 			return Session.get('currentState');
  	}
 });
 
   Template.counter.events({
-    'click #start': function (event) {
+    'click #startStop': function (event) {
     	event.preventDefault();
-      Session.set("usedHours", 0);
-		Session.set("usedMinutes", 0);
-		Session.set("usedSeconds", 0); // Will be removed later
-      // Call method to set counter state
-      Meteor.call('setCounterState');
-    },
-    'click #stop': function (event) {
-    	event.preventDefault();
-      stopp = new Date();
+    	var state = Session.get('currentState');
+    	if (state === 'on') {
+    		stopp = new Date();
       usedTime = new Date(stopp.getTime() - start.previousState.getTime());
-      Session.set("stop", stopp);
-      Session.set("usedHours", usedTime.getUTCHours());
-      Session.set("usedMinutes", usedTime.getMinutes());
-      Session.set("usedSeconds", usedTime.getSeconds());
+      Session.set('stop', stopp);
+      Session.set('usedHours', usedTime.getUTCHours());
+      Session.set('usedMinutes', usedTime.getMinutes());
+      Session.set('usedSeconds', usedTime.getSeconds());
       
       // Save used hours
       var usedTimeVar = stopp -start.previousState; 
@@ -51,6 +53,15 @@ if (Meteor.isClient) {
       });
       // Call server to reset counter state
       Meteor.call('clearCounterState');
+    		Session.set('currentState', 'off'); 
+    	} else {
+    		Session.set('usedHours', 0);
+			Session.set('usedMinutes', 0);
+			Session.set('usedSeconds', 0); // Will be removed later
+      	// Call method to set counter state
+      	Meteor.call('setCounterState');
+    		Session.set('currentState', 'on'); 
+    	}
     }
   });
 }
