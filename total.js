3d1cba2795c19f02820b9d23cbeb9617fbffcd80
c3jasalm <jaakko.salminen@cs.tamk.fi>
2014-11-12 16:01:40 +0200

    Issue #13: Variables not initialized if hours < 10

--- total.js
+++ total.js
@@ -9,7 +9,11 @@ if (Meteor.isClient) {
           })
       var totalMinutes = total / 1000 / 60;
       var hours = parseInt(totalMinutes / 60);
-      var minutes = totalMinutes % 60;
+      var minutes = parseInt(totalMinutes % 60);
+      
+      var hoursStr = hours.toString();
+      var minutesStr = minutes.toString();
+      
       if (hours < 10) {
         hoursStr = "0" + hours.toString();
       }
