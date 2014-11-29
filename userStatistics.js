cf924230d8eaaefbf251ebe2aaad65935316ff46
c3jasalm <jaakko.salminen@cs.tamk.fi>
2014-11-29 19:16:49 +0200

    Issue #2: User name and total hours moved above tabs

---
+++ userStatistics.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+
+    Template.userStatistics.helpers({
+  });
+
+  Template.userStatistics.events({
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
