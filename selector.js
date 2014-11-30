9e44b677e346989c2ea96d02872bc972fac3cade
Vesa <vesa.mujunen@cs.tamk.fi>
2014-11-30 16:28:11 +0200

    Issue #19: If overlaping entry then display alert, else save to collection.

--- selector.js
+++ selector.js
@@ -2,16 +2,10 @@ if (Meteor.isClient) {
 	today = new Date(); // Set current date 	
  	
  	Template.selector.helpers({
- 		'today': function () {	 //Set value to date input
- 		return today; 			//.toLocaleDateString(); next step is local time format
- 		},
- 		'dublicate': function() {
- 		return Session.get('testDublicate');
- 	}
 	});
 
 	Template.selector.rendered = function() {
-    $('.datetimepicker').datetimepicker({maxDate: new Date(), minuteStepping:5, format: 'MM/DD/YYYY HH:mm'});
+    $('.datetimepicker').datetimepicker({maxDate: new Date(), minuteStepping:5, format: 'MM/DD/YYYY HH:mm', defaultDate: new Date()});
     };
     
   	Template.selector.events({
@@ -22,38 +16,34 @@ if (Meteor.isClient) {
     		var todayVar = Date.parse(event.target.dtPicker.value); //Get value from input. Will be replaced with datepicker
     		var startVar = new Date(todayVar); // Will be replaced with datepicker
             
-            //Get minutes
-            var stopMinutes = event.target.timeSelector.value;
+		//Get minutes
+		var stopMinutes = event.target.timeSelector.value;
             
-            //Set used time
+          //Set used time
     		var stopVar = new Date(startVar.getTime() + (stopMinutes*60000)); 
     		var usedTimeVar = stopVar - startVar;
             
-            //Comment
-            var comment = event.target.comment.value;
+		//Comment
+		var comment = event.target.comment.value;
           
           // Check if entry is overlaping with other entry
 		var currentUserId = Meteor.userId();	 
 		if ((HoursList.findOne({$and: [{userId: currentUserId}, {start: { $lte: startVar }}, {stop: { $gt: startVar}}]}))
 		|| (HoursList.findOne({$and: [{userId: currentUserId}, {start: { $lte: stopVar }}, {stop: { $gte: stopVar}}]}))
 		|| (HoursList.findOne({$and: [{userId: currentUserId}, {start: { $gte: startVar }}, {stop: { $lte: stopVar}}]}))) {
-			Session.set('testDublicate', 'yes');
+			// Display alert if there is overlaping
 			alert('Your new entry is overlaping with some other entry');
 		}	
 		else {	
-			Session.set('testDublicate', 'no');
-		};
-	
-      	
-            
-            //Save to DB
+			//Save to DB if no overlaping
      		HoursList.insert({
-     				userId: currentUserId,
+     			userId: currentUserId,
       			start: startVar,
       			stop: stopVar,
       			usedTime: usedTimeVar,
-                comment: comment
-    		});
+                	comment: comment
+    			});
+		};
     	}
   	});
 }
