2e5a5eab10d406ac55f96c2fb9941e3647302e6e
Vesa <vesa.mujunen@cs.tamk.fi>
2014-12-08 11:20:20 +0200

    Issue #32: displays name from github profile

--- header.js
+++ header.js
@@ -1,12 +1,22 @@
 if (Meteor.isClient) {
 
 	Template.header.helpers({
-		'avatarUser':function () {
+		'avatarUser': function () {
 		var pathVar = Meteor.user().services.github.username;
 		return 'https://avatars.githubusercontent.com/' + pathVar + '?s=200';
 		},
-		currentUserName: function () {
+		'currentUserName': function () {
 		return Meteor.user().services.github.username;
-    	}	
+    	},
+    	'currentUserRealName': function () {
+    	var pathVar = Meteor.user().services.github.username;
+    	var apiPath = "https://api.github.com/users/" + pathVar;
+    	
+    	$.get(apiPath,function(data,status){
+      	var fullName = _.pick(data, 'name');
+      	Session.set('realName', fullName.name);
+    	});
+		return Session.get('realName');
+		}
 	});
 }
