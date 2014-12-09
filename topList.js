49e4a232448e10a8307b968f0e2710eb3445f09d
c3jasalm <jaakko.salminen@cs.tamk.fi>
2014-12-09 20:05:05 +0200

    Issue #34: Top List: The Beginning

---
+++ topList.js
@@ -0,0 +1,21 @@
+if (Meteor.isClient) {
+
+  Template.topList.helpers({
+    'previousHours': function () {
+    	var currentUserId = Meteor.userId();
+      return HoursList.find({userId: currentUserId}, {sort: {start: -1} })
+    },
+    'formatDate': function(date) {
+  	return moment(date).format('DD.MM.YYYY HH:mm');
+  	},
+  	'formatTime': function(time) {
+  	return moment.utc(time).format('HH:mm:ss');
+  	}
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
