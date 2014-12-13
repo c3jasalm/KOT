e2b61ed2b71f9506803634db111e8de7a19f69c8
c3jasalm <jaakko.salminen@cs.tamk.fi>
2014-12-13 12:05:33 +0200

    Issue #35: Update only at login, alert if name is not defined, move code to kot.js

--- header.js
+++ header.js
@@ -8,40 +8,12 @@ if (Meteor.isClient) {
 		},
 		'currentUserName': function () {
             return Meteor.user().services.github.username;
-    	},
-		'updateName': function () {
-            updateUsersRealName();
     	}
-        
 	});
     Template.registerHelper("currentUserRealName",function(){
-        //updateUsersRealName();
     	return getUsersRealName();
     });
-    
-    updateUsersRealName = function() {
-        console.log("SET USER INFO");
-        var currentUserId = Meteor.user().services.github.username;
-        //Check if user exists
-        var current = userInformation.findOne({_id: currentUserId});
-        var realName = getUsersRealName();
-        console.log("REAL: " + realName);
-        
-        if (current) {
-            console.log("UPDATE");
-            var newInfo = {name: realName};
-            userInformation.update( {_id: currentUserId}, { $set: newInfo });
-        } else {
-            console.log("ADD");
-            if (typeof(realName) === "undefined") {
-                realName = " ";
-            }
-            userInformation.insert({
-     			_id: currentUserId,
-      			name: realName
-    		});
-        }
-    }
+
     getUsersRealName = function() {
         var pathVar = Meteor.user().services.github.username;
     	var apiPath = "https://api.github.com/users/" + pathVar;
--- kot.js
+++ kot.js
@@ -1,7 +1,40 @@
 HoursList = new Mongo.Collection('hours');
 userInformation = new Mongo.Collection('userInfo');
 if (Meteor.isClient) {
-
+    Meteor.startup(function(){
+        Hooks.init();
+    });
+    
+     Hooks.onLoggedIn = function () {
+        if (Meteor.user()) {
+            console.log("SET USER INFO");
+            var currentUserId = Meteor.user().services.github.username;
+            //Check if user exists
+            var current = userInformation.findOne({_id: currentUserId});
+            var realName = getUsersRealName();
+            console.log("REAL: " + realName);
+            
+            if (current) {
+                console.log("UPDATE");
+                if (typeof(realName) === "undefined") {
+                    realName = " ";
+                    alert('Name not set in GitHub account. Please set.');
+                }
+                var newInfo = {name: realName};
+                userInformation.update( {_id: currentUserId}, { $set: newInfo });
+            } else {
+                console.log("ADD");
+                if (typeof(realName) === "undefined") {
+                    realName = " ";
+                    alert('Name not set in GitHub account. Please set.');
+                }
+                userInformation.insert({
+                    _id: currentUserId,
+                    name: realName
+                });
+            }
+        }    
+     }
 }
 
 if (Meteor.isServer) {
