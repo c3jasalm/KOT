ae0daae498c38123c892965440b859f95485f2cc
c3jasalm <jaakko.salminen@cs.tamk.fi>
2014-12-17 20:32:54 +0200

    Issue #34: Fix getting users real name from GitHub

--- header.js
+++ header.js
@@ -11,17 +11,30 @@ if (Meteor.isClient) {
     	}
 	});
     Template.registerHelper("currentUserRealName",function(){
-    	return getUsersRealName();
+    	return Session.get('realName');
     });
 
     getUsersRealName = function() {
         var pathVar = Meteor.user().services.github.username;
     	var apiPath = "https://api.github.com/users/" + pathVar;
     	
+        /*
     	$.get(apiPath,function(data,status){
             var fullName = _.pick(data, 'name');
             Session.set('realName', fullName.name);
     	});
+        */
+        $.ajax({
+            async: false,
+            type: 'GET',
+            url: apiPath,
+            success: function(data) {
+                var fullName = _.pick(data, 'name');
+                Session.set('realName', fullName.name)
+                console.log("Set session name: " + fullName.name);
+            }
+        });
+        
 		return Session.get('realName');
     }
             
