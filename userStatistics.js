303629256570b91cdd7546219e9e2a8df760c017
c3jasalm <jaakko.salminen@cs.tamk.fi>
2014-12-21 08:40:23 +0200

    Issue #28: Switch to bootstraps progressbar

--- userStatistics.js
+++ userStatistics.js
@@ -3,19 +3,31 @@ if (Meteor.isClient) {
     Template.userStatistics.helpers({
         percentage: function () {
             return percentageOfHours();
+        },
+        color: function () {
+            return getBarColor();
         }
   });
 
   Template.userStatistics.events({
   });
   
-  percentageOfHours = function() {
+    percentageOfHours = function() {
         var goal = 85;
         var current = totalFloat();
         var percentage = current / goal * 100;
         return Math.round(percentage);
     }  
-    
+    getBarColor = function() {
+        var percentage = percentageOfHours();
+        if (percentage < 25) {
+            return "progress-bar-danger";
+        } else if (percentage < 75) {
+            return "progress-bar-warning";
+        } else {
+            return "progress-bar-success";
+        }
+    }
     Template.userStatistics.rendered = function() {
         $( "#progressbar" ).progressbar({
             value: percentageOfHours()
