192e2627cb7466d09bffe0fa3968214c63dea8d2
Vesa <vesa.mujunen@cs.tamk.fi>
2014-12-30 17:07:38 +0200

    Issue #25: Start time fix.

--- selector.js
+++ selector.js
@@ -1,13 +1,13 @@
 if (Meteor.isClient) {
 	today = new Date(); // Set current date 	
+	rounding = 1000 * 60 * 15;
  	
  	Template.selector.helpers({
 	});
 
 	// DateTimePicker for selecting task start date and time
 	Template.selector.rendered = function() {
-	 //$('.datetimepicker').datetimepicker({maxDate: new Date(), minuteStepping:15, format: 'MM/DD/YYYY HH:mm', defaultDate: new Date()});
-    $('.datetimepicker').datetimepicker({maxDate: new Date(), minuteStepping:15, format: 'DD.MM.YYYY HH:mm', defaultDate: new Date()});
+		$('.datetimepicker').datetimepicker({maxDate: new Date(), minuteStepping:15, format: 'DD.MM.YYYY HH:mm', defaultDate: new Date(Math.round(today / rounding) * rounding)});
     };
     
   	Template.selector.events({
@@ -15,7 +15,6 @@ if (Meteor.isClient) {
 	'submit form': function (event) {
     		event.preventDefault();
     		var currentUserId = Meteor.userId();
-    		//var todayVar = Date.parse(event.target.dtPicker.value); //Get value from datepicker
     		var todayVar = moment(event.target.dtPicker.value, 'DD.MM.YYYY HH:mm'); //Get value from datepicker
     		var startVar = new Date(todayVar); 
             
